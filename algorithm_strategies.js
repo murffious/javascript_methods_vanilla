c55ba39b9129f43079806cdcbf4ff263cbc6e6c7
Paul Murff <titan.murff@gmail.com>
2019-02-09 21:50:19 -0700

    Update algorithm_strategies.js

--- algorithm_strategies.js
+++ algorithm_strategies.js
@@ -1,1 +1,11 @@
-Remove Zeros Level 4 Kata and some interviews I have had - no methods and or temp arrays etc - mainly SWAP is first item of discussion 
+//Remove Zeros Level 4 Kata and some interviews I have had 
+//w/- no methods and or temp arrays etc - mainly SWAP is first item of discussion 
+
+
+//what are strategies for solivng algorithm probelms also keeping in mind efficients and perfomrance (jsperf site)
+
+
+// SORT broken down 
+// SWAP step
+// Compare step//
+
