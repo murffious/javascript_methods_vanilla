ba9c7585bdb38b5fdff75827118f9afab1b872f6
Paul Murff <titan.murff@gmail.com>
2019-02-09 21:54:20 -0700

    Update algorithm_strategies.js

--- algorithm_strategies.js
+++ algorithm_strategies.js
@@ -1,6 +1,6 @@
 //Remove Zeros Level 4 Kata and some interviews I have had 
 //w/- no methods and or temp arrays etc - mainly SWAP is first item of discussion 
-
+// TIPS - MIT algoeithm class ideas here to oin basic way 
 
 //what are strategies for solivng algorithm probelms also keeping in mind efficients and perfomrance (jsperf site)
 
